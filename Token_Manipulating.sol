90dfc2959babad02791a2e8852823205e04f85de
ArunKumar Shanmugam <arunsathamk@gmail.com>
2018-04-23 17:18:41 +0530

    Update Token_Manipulating.sol

--- Token_Manipulating.sol
+++ Token_Manipulating.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.13;
 
 contract ERC20 {
     function totalSupply() public constant returns (uint);
+    function name() public constant returns (string _name);
+    function symbol() public constant returns (string _symbol);
     function balanceOf(address tokenOwner) public constant returns (uint balance);
     function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
     function transfer(address to, uint tokens) public returns (bool success);
@@ -25,6 +27,18 @@ contract Token{
     // before this You need to run approve function on your token contract...
     function transfertoken(address _token,address _to,uint _amt) public payable {
         ERC20(_token).transferFrom(msg.sender,_to,_amt);
+    }
+    
+    function transfer(address _token,address _to,uint _amt) public payable {
+        ERC20(_token).transfer(_to,_amt);
+    }
+    
+    function name(address _token) public constant returns(string){
+        return ERC20(_token).name();
+    }
+    
+     function symbol(address _token) public constant returns(string){
+        return ERC20(_token).symbol();
     }
     
 }
